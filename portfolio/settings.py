1cdc4dbabe49b6c762fb1199b14fba93894dba6a
fred <frederickchute@gmail.com>
2018-10-05 03:05:02 -0400

    security

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,3 +134,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #Saved to root
 MEDIA_URL = '/media/'
 #Where they can access
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
