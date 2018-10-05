a42282e7b04b6d15a0c50deafa7a2093dc5ffa74
fred <frederickchute@gmail.com>
2018-10-05 00:25:16 -0400

    Jobs done

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    link = models.URLField()
     #django model field reference
