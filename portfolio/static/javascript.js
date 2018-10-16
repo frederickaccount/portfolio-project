1f4abed23c23376bb547951b9d08ab9a0c6ee9e3
fred <frederickchute@gmail.com>
2018-10-16 00:16:53 -0400

    Added a simple js alert to contact form

--- portfolio/static/javascript.js
+++ portfolio/static/javascript.js
@@ -1,9 +1,9 @@
 function confirmation() {
-	var answer = confirm("Thanks for contacting me! Want to see my blog? (Just showing off javascript here)")
-	if (answer == true){
-		window.location.href = "http://localhost:8000/blog/";
-	}
-	else{
-		alert("Thanks again!");
-	}
+	alert('Thanks for contacting me!');
+	// if (answer == true){
+	// 	window.location.href = "http://localhost:8000/blog/";
+	// }
+	// else{
+	// 	alert("Thanks again!");
+	// }
 }
